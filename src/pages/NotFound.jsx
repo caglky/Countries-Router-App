3872edf16dee4d4cfe17a553ab949a83c611e139
Bedirhan KARA <bedirhank.bk41@gmail.com>
2026-04-14 00:10:52 +0300

    conflict2

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,8 +4,8 @@ export default function NotFound(){
     return (
         <div>
             <h1>404 - Sayfa Bulunamadi Sayfada Hata var route değiştiriniz !</h1>
-            <p> There is no such kind of pages</p>
-            <Link to = "/">Return the main pages</Link>
+            <p> There is no such kind of pages hayır conflit olmasın</p>
+            <Link to = "/">Return the main pages olamaaaz olamaaaz dedimmm</Link>
         </div>
     );
 }
